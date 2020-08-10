7e0a7bf6f8d149c6156786fb3fd3b7f3cd15d056
whoami <admin@imipy.com>
2020-08-10 11:23:37 +0800

    fix: urls

--- flask_mico/urls.py
+++ flask_mico/urls.py
@@ -1,4 +1,5 @@
 from importlib import import_module
+from inspect import isclass, isfunction
 from types import ModuleType
 from urllib.parse import urljoin
 
@@ -12,7 +13,7 @@ def include(mod_path: str):
 
 def handle_app_urls(prefix: str, urls_mod):
     for url in urls_mod.urlpatterns:
-        if not issubclass(url[1], ApiView):
+        if not (isclass(url[1]) and issubclass(url[1], ApiView)):
             logger.error("'%s' Not an instance of 'ApiView'", str(url[1]))
             break
         path = urljoin(prefix, url[0])
@@ -27,8 +28,11 @@ def register_routes(app, urls_path: str):
     urls_mod = import_module(urls_path)
     logger.debug('register routes %s', urls_mod.urlpatterns)
     for url_ in urls_mod.urlpatterns:
-        if isinstance(url_[1], ApiView):
-            add_url_rule(url_[0], url_[1])
-        elif isinstance(url_[1], ModuleType):
+        if isinstance(url_[1], ModuleType):
             for path, view in handle_app_urls(url_[0], url_[1]):
                 add_url_rule(path, view)
+        elif isclass(url_[1]) and issubclass(url_[1], ApiView):
+            add_url_rule(url_[0], url_[1])
+        elif isfunction(url_[1]):
+            app.add_url_rule(url_[0], endpoint=url_[
+                             1].__name__, view_func=url_[1])
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="flask_mico",
-    version="0.2.2",
+    version="0.2.3",
     include_package_data=True,
     description="flask mico",
     long_description=long_description,
